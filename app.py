44d01df2886ccc902f9691bf23dc376c02573880
AbdallaAbker <aabdalla1088@gmail.com>
2023-12-11 04:08:02 -0700

    testing logging & custom exception

---
+++ app.py
@@ -0,0 +1,12 @@
+from src.logging import logger
+from src.exception import CustomException
+import sys
+
+
+if __name__ == "__main__":
+    logger.info("Started")
+
+    try:
+       a = 1/0
+    except Exception as e:
+        raise CustomException(e, sys)
--- src/logging/__init__.py
+++ src/logging/__init__.py
@@ -6,7 +6,7 @@ from datetime import datetime
 logging_str = "[%(asctime)s: %(levelname)s: %(filename)s: %(module)s: %(message)s]"
 log_dir= "logs"
 datetime_str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
-log_file_name = f"{datetime_str} + _fire_forest_prediction.log"
+log_file_name = f"{datetime_str}_fire_forest_prediction.log"
 log_filepath = os.path.join(log_dir, log_file_name)
 os.makedirs(log_dir, exist_ok=True)
 
